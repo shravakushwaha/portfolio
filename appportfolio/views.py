c87912e5b683a55ddf127c94e7fbf49d7e5a88ad
shravan <shravan@faarms.com>
2021-12-12 19:23:24 +0530

    All portfolio data

---
+++ appportfolio/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(args):
+    context = {}
+    return render(args,'appportfolio/home.html')
+
+def resume(request):
+    context = {}
+    return render(request, 'appportfolio/resume.html', context)
+def experience(request):
+    context = {}
+    return render(request, 'appportfolio/experience.html', context)
+
